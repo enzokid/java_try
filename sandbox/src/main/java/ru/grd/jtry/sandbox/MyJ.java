907472dd2087e570018fd775c635862c73d33e36
enzokid <overstill77@gmail.com>
2021-01-20 12:49:31 +0200

    Update MyJ.java

--- sandbox/src/main/java/ru/grd/jtry/sandbox/MyJ.java
+++ sandbox/src/main/java/ru/grd/jtry/sandbox/MyJ.java
@@ -1,7 +1,7 @@
 package ru.grd.jtry.sandbox;
 
-public  class MyJ {
-	public static void main (String[] args){
-		System.out.println("Hello, world");
-	}
+public class MyJ {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
 }
