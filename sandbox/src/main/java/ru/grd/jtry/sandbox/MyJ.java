26921ea87f971147211838348822735e4dd41cce
enzokid <overstill77@gmail.com>
2021-01-22 12:30:05 +0200

    Some functiobs added

--- sandbox/src/main/java/ru/grd/jtry/sandbox/MyJ.java
+++ sandbox/src/main/java/ru/grd/jtry/sandbox/MyJ.java
@@ -2,6 +2,29 @@ package ru.grd.jtry.sandbox;
 
 public class MyJ {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+
+        hello("world");
+        hello("user");
+        hello("vitaly");
+        double l=5;
+        System.out.println("площадь квадрата со стороной "+ l + "="+ area(l));
+
+        double a=4;
+        double b=6;
+        System.out.println("площадь прмоугольника со сторонами "+ a +" и " +b+" = "+ area(a,b));
+    }
+
+    public static  void  hello (String somebody) {
+
+
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area(double len){
+        return len*len;
+    }
+
+    public static double area(double a, double b){
+        return  a*b;
     }
 }
